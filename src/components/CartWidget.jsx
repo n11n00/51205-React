a2e3e665c8a5b7d86da7c3cb7f333b403dcfbc7f
unknown <nino.f.martinez@gmail.com>
2023-03-27 19:14:07 -0300

    first commit

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+function CartWidget() {
+    return (
+      <>
+        <button type="button" className="btn btn-primary">
+            <img src="../img/cart.ico" alt="" />
+            <span className="badge text-bg-secondary">4</span>
+                </button>
+                
+      </>
+    );
+  }
+  
+  export default CartWidget;
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function ItemListContainer(props) {
+    return (
+      <>
+        <h1>{props.saludo}</h1>
+      </>
+    );
+  }
+  
+  export default ItemListContainer;
