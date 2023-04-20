7d6510915e42eae4829a697c4653a5ebaf014b49
unknown <nino.f.martinez@gmail.com>
2023-04-20 19:17:57 -0300

    comit373

--- src/components/OptionNavbarComponent.jsx
+++ src/components/OptionNavbarComponent.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { routes } from "../utils/Routes";
+
 
 
 const OptionNavbarComponent = (props) => {
-        const {titleOption,link} = props;
+        const {titleOption} = props;
         const isActiveClass = ({isActive}) =>{
             return isActive ? 'active nav-link' : 'nav-link';
         };
 
     return (
-        <NavLink to={`/category/${titleOption}`}  className={isActiveClass}>
+        <NavLink to={`${routes.categoryView.replace(":category",titleOption)}`}  className={isActiveClass}>
             {titleOption}
         </NavLink>
     );
